83d85965458bc4e68b35e3d26c8ba89ee6bb3dc4
Junichi TANAKA <junsan45@gmail.com>
2020-03-19 02:41:37 +0900

    Try to use appProperties

--- list.go
+++ list.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/pkg/errors"
 	"google.golang.org/api/drive/v3"
@@ -21,7 +21,7 @@ var (
 
 const (
 	// https://developers.google.com/drive/api/v3/search-files
-	driveQuery = "'%s' in parents and mimeType = 'text/csv'"
+	driveQuery = "'%s' in parents and mimeType = 'text/csv' and not appProperties has { key='development' and value = 'done' }"
 	driveFetchSize = 100
 )
 
@@ -38,7 +38,7 @@ func main() {
 	}
 
 	query := fmt.Sprintf(driveQuery, driveFolderId)
-	fl, err := driveService.Files.List().Q(query).PageSize(driveFetchSize).Do()
+	fl, err := driveService.Files.List().Q(query).Fields("files(id,name,mimeType,size,createdTime,modifiedTime,appProperties)").OrderBy("modifiedTime").PageSize(driveFetchSize).Do()
 	if err != nil {
 		if gerr, ok := err.(*googleapi.Error); ok {
 			if gerr.Code == http.StatusNotFound {
@@ -51,7 +51,18 @@ func main() {
 
 	for _, f := range fl.Files {
 		fmt.Println(f.Name)
-		_ = download(ctx, f)
+		fmt.Println(f.Properties)
+		fmt.Println(f.AppProperties)
+		fmt.Println(f.ModifiedTime)
+		//_ = download(ctx, f)
+		fmt.Println(time.Now())
+		ff := &drive.File{}
+		ff.AppProperties = make(map[string]string)
+		ff.AppProperties["development"] = "done"
+		_, err := driveService.Files.Update(f.Id, ff).Context(ctx).Do()
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
